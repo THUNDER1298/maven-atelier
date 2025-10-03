575a585c78e18465d921832cd860da4bc1511f54
hamza <hamzabencheikh8@gmail.com>
2025-10-03 19:19:07 +0100

    TEST: Déclenchement automatique de Jenkins

--- product-service/src/main/java/com/example/product/service/ProductService.java
+++ product-service/src/main/java/com/example/product/service/ProductService.java
@@ -2,8 +2,7 @@ package com.example.product.service;
 
 public class ProductService {
     public String getProductName() {
-        // Dans une vraie application, ce code irait chercher des données
-        // dans une base de données.
+        // Un petit commentaire pour tester Jenkins
         return "Ordinateur Ultra-Puissant X500";
     }
 }
